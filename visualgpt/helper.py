a91517caed5209388169b935259783b4c60e8051
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-05-11 18:10:21 +0800

    Update helper.py

--- visualgpt/helper.py
+++ visualgpt/helper.py
@@ -12,9 +12,9 @@ def blip2_vision_processor(model='eval'):
     
     return vis_processors[model]
 
-def get_vision_position(input_ids, vision_token_id):
-    mask_value = input_ids.shape[1]
-    vision_position = torch.arange(input_ids.shape[-1])
-    vision_position = repeat(vision_position, 'n -> b n', b=input_ids.shape[0])
-    vision_position = vision_position.masked_fill(input_ids!=vision_token_id, mask_value).sort(dim=-1).values
-    return vision_position
+# def get_vision_position(input_ids, vision_token_id):
+#     mask_value = input_ids.shape[1]
+#     vision_position = torch.arange(input_ids.shape[-1])
+#     vision_position = repeat(vision_position, 'n -> b n', b=input_ids.shape[0])
+#     vision_position = vision_position.masked_fill(input_ids!=vision_token_id, mask_value).sort(dim=-1).values
+#     return vision_position
