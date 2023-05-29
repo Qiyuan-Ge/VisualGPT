9f5fb935d219aec9e121f800c04aca54cd6d6d6f
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-05-29 19:52:15 +0800

    Update helper.py

--- visualgpt/helper.py
+++ visualgpt/helper.py
@@ -1,20 +1,12 @@
-import os
 import torch
-from einops import repeat
-from omegaconf import OmegaConf
-from lavis.models import load_preprocess
+from transformers import BlipImageProcessor
 
-cur_dir = os.path.dirname(os.path.abspath(__file__))
-
-def blip2_vision_processor(model='eval'):
-    cfg = OmegaConf.load(os.path.join(cur_dir, "blip2_pretrain.yaml"))
-    vis_processors, _ = load_preprocess(cfg.preprocess)
-    
-    return vis_processors[model]
-
-# def get_vision_position(input_ids, vision_token_id):
-#     mask_value = input_ids.shape[1]
-#     vision_position = torch.arange(input_ids.shape[-1])
-#     vision_position = repeat(vision_position, 'n -> b n', b=input_ids.shape[0])
-#     vision_position = vision_position.masked_fill(input_ids!=vision_token_id, mask_value).sort(dim=-1).values
-#     return vision_position
+class ImageProcessor:
+    def __init__(self, mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711], size={'height': 224, 'width': 224}):
+        self.processor = BlipImageProcessor(image_mean=mean, image_std=std, size=size)
+        
+    def __call__(self, image):
+        image = self.processor(image)['pixel_values'][0]
+        image = torch.tensor(image)
+        
+        return image
